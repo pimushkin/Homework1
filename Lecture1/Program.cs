0f6af53f08545f7c9553fae17ba40a246c8b8e66
Dmitry Pimushkin <d.pimushkin@gmail.com>
2024-04-19 19:11:26 +0200

    Initial commit

---
+++ Lecture1/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool isActive = false;
+
+        if (isActive)
+        {
+            Console.WriteLine("Активен");
+        }
+        else
+        {
+            Console.WriteLine("Неактивен");
+        }
+    }
+}
